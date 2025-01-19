1d53c51d5b381ef19dfdd383be7396c53bccdbd1
WcOnR <19wconr93@gmail.com>
2025-01-20 01:08:37 +0400

    added attack animation

--- game_objects/enemies/enemy.gd
+++ game_objects/enemies/enemy.gd
@@ -1,8 +1,11 @@
 class_name Enemy extends Node2D
 
 @onready var health_comp : HealthComp = $HealthComp
+@export var attack_anim_curve : Curve
+
 var enemy_data : EnemyData = null
 var next_action : Action = null
+var anim : AnimObject = null
 
 const ON_PLAN : StringName = "on_plan"
 const ON_ACTION : StringName = "on_action"
@@ -20,5 +23,14 @@ func plan_next_attack(line : BattleLine) -> void:
 	next_action.run(ON_PLAN, [enemy_data, line])
 
 
+func _process(delta: float) -> void:
+	if anim:
+		anim.update(delta)
+
+
 func attack(player : Player):
+	anim = AnimObject.new()
+	anim.initialize(self, attack_anim_curve, 0.25)
+	await anim.anim_finished
+	anim = null
 	await next_action.run(ON_ACTION, [enemy_data, player])
