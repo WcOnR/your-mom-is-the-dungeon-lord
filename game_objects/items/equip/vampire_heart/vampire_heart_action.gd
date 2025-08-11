70b34e0a2a14c02d56c9ccca2a93e8397847d1ef
WcOnR <19wconr93@gmail.com>
2025-08-12 01:24:00 +0400

    fix small bugs

--- game_objects/items/equip/vampire_heart/vampire_heart_action.gd
+++ game_objects/items/equip/vampire_heart/vampire_heart_action.gd
@@ -10,7 +10,9 @@ func on_attack_applied(_player : Player, _level : int) -> void:
 
 
 func on_battle_start(_player : Player, _level : int) -> void:
-	_to_type.actions.actions[0].args = [0, 20]
+	var gem := _to_type.action as GemSharpHeart
+	gem.base_attack = 20
+	gem.base_heal = 0
 	var _field := _player.get_tree().get_first_node_in_group("Field") as Field
 	var _gem_set := _field.gem_set.duplicate() as GemSet
 	_gem_set.gem_types = _gem_set.replace_gem(_from_type, _to_type)
